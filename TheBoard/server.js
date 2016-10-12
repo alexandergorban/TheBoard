5ff804a06624d9f9d11cd760ab95b2c1c1917a48
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-12 15:30:49 +0300

    View Engines - EJS

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -1,14 +1,17 @@
 ﻿var http = require("http");
 var express = require("express");
 var app = express();
+var ejsEngine = require("ejs-locals");
 
 // Setup the View Engine
-app.set("view engine", "jade");
+//app.set("view engine", "jade");
+app.engine("ejs", ejsEngine); // support master pages
+app.set("view engine", "ejs"); // ejs view engine
 
 app.get("/",
     function (req, res) {
     //res.send("<html><body><h1>Express</h1></body></html>");
-    res.render("jade/index", { title: "Express + Jade" });
+    res.render("ejs/index", { title: "Express + EJS" });
 });
 
 app.get("/api/users",
