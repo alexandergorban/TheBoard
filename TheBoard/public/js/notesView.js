f188081efaccdbfbf18deaccc62a2df0428a2880
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-14 15:55:43 +0300

    AngularJS. Calling the API (/api/notes/:categoryName)

--- TheBoard/public/js/notesView.js
+++ TheBoard/public/js/notesView.js
@@ -4,30 +4,24 @@
     var theModule = angular.module("notesView", []);
     
     theModule.controller("notesViewController", 
-        ["$scope",
-        function ($scope) {
-            $scope.notes = [
-                {
-                    note: "Hello World",
-                    color: "yellow",
-                    author: "AngularClass"
-                },
-                {
-                    note: "Hello World 2",
-                    color: "blue",
-                    author: "AngularClass"
-                },
-                {
-                    note: "Hello World 3",
-                    color: "green",
-                    author: "AngularClass"
-                },
-                {
-                    note: "This is from JS code",
-                    color: "orange",
-                    author: "AngularClass"
-                }
-            ];
+        ["$scope", "$window", "$http",
+        function ($scope, $window, $http) {
+            $scope.notes = [];
+
+            // Get the categore name
+            var urlParts = $window.location.pathname.split("/");
+            var categoryName = urlParts[urlParts.length - 1];
+
+            var notesUrl = "/api/notes/" + categoryName;
+            $http.get(notesUrl)
+                .then(function(result) {
+                    // success
+                    $scope.notes = result.data;
+                }, function(err) {
+                    // error
+                    console.log(err);
+                });
+
         }]);
 
 })(window.angular);
