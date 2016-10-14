4e3edd32117c8781d41f935ac9d7403aa0c02c79
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-14 19:04:33 +0300

    Implementing the Register Page.

--- TheBoard/auth/index.js
+++ TheBoard/auth/index.js
@@ -1,11 +1,36 @@
 ﻿// auth/index.js
-(function(auth) {
-
-    auth.init = function(app) {
+(function (auth) {
+    
+    var data = require("../data");
+    
+    auth.init = function (app) {
+        
         app.get("/register",
-            function(req, res) {
-                res.render("register", { title: "Register for The Board" });
+            function (req, res) {
+            res.render("register", { title: "Register for The Board", message: req.flash("registrationError") });
+        });
+        
+        app.post("/register",
+            function (req, res) {
+            var user = {
+                name: req.body.name,
+                email: req.body.email,
+                username: req.body.username,
+                passwordHash: "",
+                salt: ""
+            };
+            
+            data.addUser(user,
+                    function (err) {
+                if (err) {
+                    req.flash("registrationError", "Could not save user to database.");
+                    req.redirect("/register");
+                } else {
+                    res.redirect("/login");
+                }
             });
+        });
+
     };
 
 })(module.exports);
