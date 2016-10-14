bc13c8c20db182314ff7d9f7c04c3466b425cd5e
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-14 17:50:33 +0300

    AngularJS. Calling the API to Created a Note.

--- TheBoard/public/js/notesView.js
+++ TheBoard/public/js/notesView.js
@@ -7,25 +7,41 @@
         ["$scope", "$window", "$http",
         function ($scope, $window, $http) {
             $scope.notes = [];
-            $scope.newNote = {
-                note: "",
-                color: "yellow"
-            };
-
+            $scope.newNote = createBlankNote();
+            
             // Get the categore name
             var urlParts = $window.location.pathname.split("/");
             var categoryName = urlParts[urlParts.length - 1];
-
+            
             var notesUrl = "/api/notes/" + categoryName;
             $http.get(notesUrl)
-                .then(function(result) {
+                .then(function (result) {
+                // success
+                $scope.notes = result.data;
+            }, function (err) {
+                // error
+                console.log(err);
+            });
+            
+            $scope.save = function () {
+                $http.post(notesUrl, $scope.newNote)
+                    .then(function (result) {
                     // success
-                    $scope.notes = result.data;
-                }, function(err) {
-                    // error
-                    console.log(err);
+                    $scope.notes.push(result.data);
+                    $scope.newNote = createBlankNote();
+                }, function (err) {
+                        // failure
+                        // TODO
                 });
+            };
 
         }]);
+    
+    function createBlankNote() {
+        return {
+            note: "",
+            color: "yellow"
+        };
+    };
 
 })(window.angular);
