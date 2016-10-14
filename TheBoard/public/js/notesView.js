375f37f5ad3699bd4f84fd186230b28cea4449a2
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-14 15:40:32 +0300

    Creating the Controller (notesView).

--- TheBoard/public/js/notesView.js
+++ TheBoard/public/js/notesView.js
@@ -1,6 +1,33 @@
 ﻿// notesView.js
-(function(angular) {
-
+(function (angular) {
+    
     var theModule = angular.module("notesView", []);
+    
+    theModule.controller("notesViewController", 
+        ["$scope",
+        function ($scope) {
+            $scope.notes = [
+                {
+                    note: "Hello World",
+                    color: "yellow",
+                    author: "AngularClass"
+                },
+                {
+                    note: "Hello World 2",
+                    color: "blue",
+                    author: "AngularClass"
+                },
+                {
+                    note: "Hello World 3",
+                    color: "green",
+                    author: "AngularClass"
+                },
+                {
+                    note: "This is from JS code",
+                    color: "orange",
+                    author: "AngularClass"
+                }
+            ];
+        }]);
 
 })(window.angular);
